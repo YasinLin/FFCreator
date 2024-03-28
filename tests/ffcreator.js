6de1e028d058da65b3da3b436f20836a02a555c2
yasin <yasin>
2024-03-28 10:48:55 +0800

    转场为空处理

--- tests/ffcreator.js
+++ tests/ffcreator.js
@@ -3,7 +3,11 @@ const colors = require('colors');
 const path = require("path");
 const fs = require('fs');
 
-const vs = new FFExector({   });
+const vs = new FFExector({
+  render: 'gl',
+  ffmpeg: '/Volumes/data/data/program/nodejs/woqivc/addon/ffmpeg',
+  ffprobe: '/Volumes/data/data/program/nodejs/woqivc/addon/ffprobe',
+});
 const file = fs.readFileSync(path.resolve(__dirname, "./test.json"));
 vs.sync(JSON.parse(file));
 vs.on('start', () => {
